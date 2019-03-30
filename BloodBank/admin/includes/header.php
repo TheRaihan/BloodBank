fc52d8880ca27d69665b9560ce33cebec32f27f5
Raihan Taher <36502480+TheRaihan@users.noreply.github.com>
2019-03-31 02:49:25 +0600

    Add files via upload

---
+++ BloodBank/admin/includes/header.php
@@ -0,0 +1,15 @@
+<div class="brand clearfix">
+	<div class="">ds	</div>
+	<a href="dashboard.php" style="font-size: 20px; padding-left:10%  ; color:#fff;" >Blood Donation System </a>
+		<span class="menu-btn"><i class="fa fa-bars"></i></span>
+		<ul class="ts-profile-nav">
+
+			<li class="ts-account">
+				<a href="#"><img src="img/ts-avatar.jpg" class="ts-avatar hidden-side" alt=""> Admin <i class="fa fa-angle-down hidden-side"></i></a>
+				<ul>
+					<li><a href="change-password.php">Change Password</a></li>
+					<li><a href="logout.php">Logout</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
---
+++ BloodBank/admin/includes/leftbar.php
@@ -0,0 +1,13 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+
+				<div class="">d	</div>
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+
+				<li><a href="add-donor.php"><i class="fa fa-edit"></i> Add Donor</a></li>
+
+				<li><a href="donor-list.php"><i class="fa fa-users"></i> Donor List</a></li>
+
+			</ul>
+		</nav>
